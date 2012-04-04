ed02cb179756c6926accf66368c5cd05f3ca63e9
Ivan Matyunin <sibrus@gmail.com>
2012-04-04 14:44:14 -0400

    Test commit

--- lib/Twig/Extensions/Extension/Layout.php
+++ lib/Twig/Extensions/Extension/Layout.php
@@ -4,7 +4,7 @@ class Twig_Extensions_Extension_Layout extends Twig_Extension
 {
     public function getFunctions()
     {
-    	//not even close to done yet
+    	//not even close to a moose
         return array(
             'desktop' => new Twig_Function_Function('desktopLayout'),
         );
